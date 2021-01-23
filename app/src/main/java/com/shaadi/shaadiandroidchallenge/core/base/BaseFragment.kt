3fcfbbd202c1d1b20ba38478e6fb16d6c550ec4f
vineed <vineedgangadharan@gmail.com>
2021-01-23 21:20:31 +0530

    added BaseFragment.kt for base frag logic

---
+++ app/src/main/java/com/shaadi/shaadiandroidchallenge/core/base/BaseFragment.kt
@@ -0,0 +1,7 @@
+package com.shaadi.shaadiandroidchallenge.core.base
+
+import androidx.annotation.LayoutRes
+import androidx.fragment.app.Fragment
+
+abstract class BaseFragment(@LayoutRes layoutId: Int) : Fragment(layoutId) {
+}
